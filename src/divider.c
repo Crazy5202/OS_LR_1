3ff486d3a0f3900dc3da8d1d11dc086829c6a27b
maxim <mdkutsenko@mai.education>
2023-10-01 23:10:54 +0300

    Changed from CPP to C

---
+++ src/divider.c
@@ -0,0 +1,58 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "string.h"
+
+int main() {
+    int len;
+    if (read(STDIN_FILENO, &len, sizeof(int)) == -1) { // reading length of data
+        perror("Forward pipe reading error");
+        _exit(-1);
+    }
+    char* data = malloc(sizeof(char)*len);
+    if (read(STDIN_FILENO, data, len) == -1) { // reading whole data
+        perror("Forward pipe reading error");
+        _exit(-1);
+    }
+    char* var = strtok(data, "\n"); // getting filename
+    int file = open(var, O_WRONLY | O_CREAT, 0777);
+    if (file == -1) {
+        perror("File reading error");
+        _exit(-2);
+    }
+    var = strtok(NULL, "\n"); // getting array with all the numbers
+    var = strtok(var, " "); // getting first number as char*
+    if (var == NULL) { // checking whether data is correct
+        _exit(-3);
+    }
+    int result=atoi(var); // getting first number as int
+    var = strtok(NULL, " ");
+    while (var != NULL) { // dividing
+        printf("%d ", result);
+        int del = atoi(var);
+        if (del == 0) { // checking for 0 div
+            _exit(-4);
+        }
+        result/=del;
+        var = strtok(NULL, " ");
+    }
+
+    int num_len=0, num = result; // creating string version of result
+    while (num>0) {
+        num/=10;
+        num_len++;
+    }
+    char res_str[num_len];
+    sprintf(res_str, "%d", result);
+
+    if ((write(file, "Here is the answer: ", 20*sizeof(char)) == -1) || (write(file, &res_str, num_len*sizeof(char)) == -1)) { // writing answer to the file
+        perror("Writing to file error");
+        _exit(-5);
+    }
+    if ((write(STDOUT_FILENO, &num_len, sizeof(int)) == -1) || (write(STDOUT_FILENO, &res_str, num_len*sizeof(char)) == -1)) { // writing to backward pipe
+        perror("Writing to backward pipe error");
+        _exit(-5);
+    }
+    return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,72 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/wait.h"
+
+int main() {
+    int pipe_fd[2]; // creating pipe to child process; pipe_fd[0] - for reading, pipe_fd[1] - for writing
+    int pipe_bw[2]; // creating pipepipe from executed program to parent process
+    if ((pipe(pipe_fd) == -1) || (pipe(pipe_bw)==-1)) { // creating pipes
+        perror("Pipe creation error");
+        return 1;
+    }
+    pid_t pid = fork(); // creating child process
+    if (pid == -1) {
+        perror("Child process creation error");
+        return 2;
+    } else if (pid == 0) { // going in child process
+        close(pipe_fd[1]); // closing the descriptor of forward pipe to write
+        close(pipe_bw[0]); // closing the descriptor of backward pipe to read
+        dup2(pipe_fd[0], STDIN_FILENO); // duplicating descriptors
+        dup2(pipe_bw[1], STDOUT_FILENO);
+        execl("divider", "divider", NULL); // executing program to divide the numbers
+        perror("Exec error");
+        return 3;
+    } else { // going in parent process
+        close(pipe_fd[0]); // closing the descriptor of forward pipe to read
+        close(pipe_bw[1]); // closing the descriptor of backward pipe to write
+        char* input = 0; // creating char* pointer for reading input
+        int len = 0;
+        int counter = 0; // creating variable for counting lines of input
+        while(counter != 2) {
+            char c;
+            if (read(STDIN_FILENO, &c, sizeof(char)) == -1) { // reading from input
+                perror("Input reading error");
+                return 4;
+            }
+            input = realloc(input, sizeof(char)*(++len));
+            input[len-1]=c;
+            if (c == '\n') {
+                counter += 1;
+            }
+        }
+        input[len-1]=' '; // doing this for strtok later
+        if ((write(pipe_fd[1], &len, sizeof(int)) == -1) || (write(pipe_fd[1], input, sizeof(char)*len) == -1)) { // writing to forward pipe
+            perror("Writing to forward pipe error");
+            return 5;
+        }
+        close(pipe_fd[1]); // closing the descriptor to write because it is no longer needed 
+        
+        int status;
+        wait(&status); // waiting for child process to finish
+        if (WIFEXITED(status) && WEXITSTATUS(status)!=0) { // checking for error in child
+            return(6);
+        }
+        int answ_len;
+        
+        if (read(pipe_bw[0], &answ_len, sizeof(int)) == -1) { // reading answer length from pipe
+            perror("Backward pipe reading error");
+            return 4;
+        }
+        char* answer = malloc(sizeof(char)*answ_len);
+        if (read(pipe_bw[0], &answer, answ_len*sizeof(char)) == -1) { // reading answer from pipe
+            perror("Backward pipe reading error");
+            return 4;
+        }
+        if ((write(STDOUT_FILENO, &answer, answ_len*sizeof(char)) == -1) || (write(STDOUT_FILENO, "\n", sizeof(char)) == -1)) { // writing answer to output
+            perror("Writing to stdout error");
+            return 5;
+        }
+    }
+    return 0;
+}
